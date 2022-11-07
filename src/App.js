e8598b08a7870d7088dd419ed75087a08756218e
alimranhossain <alimranhossain44@gmail.com>
2022-11-08 01:39:28 +0600

    folder setup and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import {RouterProvider} from 'react-router-dom';
+import {routes} from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      <h2>Future Travel</h2>
+      <RouterProvider router={routes}> </RouterProvider>
     </div>
   );
 }
