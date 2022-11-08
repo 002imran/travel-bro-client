c3b6a04b4041a98dc1a3456333f615fcc29052c8
alimranhossain <alimranhossain44@gmail.com>
2022-11-08 16:45:00 +0600

    registration page

--- src/Pages/Slider/Slider.js
+++ src/Pages/Slider/Slider.js
@@ -3,7 +3,6 @@ import slider1 from "../../assets/images/slider/cover-3.png";
 import banner2 from "../../assets/images/slider/banner2.png";
 const Slider = () => {
   return (
-  
     <section class="bg-white dark:bg-gray-900">
       <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7">
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,5 +1,7 @@
 import {createBrowserRouter} from "react-router-dom";
 import Main from "../../layout/Main";
+import Login from "../../Pages/Login/Login/Login";
+import Register from "../../Pages/Login/Register/Register";
 import Slider from "../../Pages/Slider/Slider";
 export const routes = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ export const routes = createBrowserRouter([
         path: "",
         element: <Slider></Slider>,
       },
+      {
+        path:'/login',
+        element: <Login></Login>
+      },
+      {
+        path:'/register',
+        element: <Register></Register>
+      }
     ],
   },
 ]);
